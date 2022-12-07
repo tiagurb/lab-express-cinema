a40c166fa75ec5eab4b6920ea45a0d75101ba233
tiagurb <tiagurb@hotmail.com>
2022-12-07 12:06:42 +0000

    details missing

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,16 @@ const router = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+/*const Movie = require('../models/Movie.model');
+
+router.get('/movies', async (req, res, next) => {
+  try {
+    const allMovies = await Movie.find();
+    res.render('movies', { movies: allMovies });
+  } catch (error) {
+    console.log('error', error);
+    next(error);
+  }
+});*/
+
 module.exports = router;
---
+++ routes/movies-routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const Movie = require('../models/Movie.model');
+
+router.get('/movies', async (req, res, next) => {
+  try {
+    const allMovies = await Movie.find();
+    res.render('movies', { movies: allMovies });
+  } catch (error) {
+    console.log('error', error);
+    next(error);
+  }
+});
+
+router.get('/movies/:movieId', async (req, res, next) => {
+  try {
+    // console.log(req.params);
+    const { movieId } = req.params;
+
+    const movie = await Movie.findById(movieId);
+    res.render('seeds/movies.seed', movie);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
